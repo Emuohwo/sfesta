5a97541fc2303e708c18e9e80317982b721d5f5c
Emuohwo <warriisaac@outlook.com>
2026-05-02 03:28:51 -1200

    Add generate report

--- cypress.config.js
+++ cypress.config.js
@@ -8,8 +8,17 @@ module.exports = defineConfig({
   e2e: {
   testIsolation: false,
     baseUrl: 'https://stem-admin.qwizfun.com',
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'custom-title',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     retries: {
       runMode: 1,
